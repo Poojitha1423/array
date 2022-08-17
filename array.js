141a4b59de3d7b3570965c43d4704ca1ec82a964
Poojitha <poojitha032320@gmail.com>
2022-08-17 19:56:47 +0530

    array

---
+++ array.js
@@ -0,0 +1,10 @@
+varstringArray = ["PJ","STRING","ARRAY"];
+varnumericArray = [23,45,50];
+vardecimalArray = [2.3,1.4,2.2];
+varbooleanArray = [true,false,true,false];
+varmixedArray = [2,"string",2.2];
+console.log(varstringArray)
+console.log(varnumericArray)
+console.log(vardecimalArray)
+console.log(varbooleanArray)
+console.log(varmixedArray)
